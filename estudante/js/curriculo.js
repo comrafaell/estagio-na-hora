85b8d97dc5be71677659ebba19dc2e78d8d584ca
Rafael Lima <87089861+comrafaellima@users.noreply.github.com>
2022-05-27 09:46:12 -0300

    Fazendo alterações no HTML, CSS e retirando o JS

--- estudante/js/curriculo.js
+++ estudante/js/curriculo.js
@@ -1,16 +1,1 @@
-function imprimir(){
-    var pegar_dados = document.getElementsById('curriculo').innerHTML;
 
-    var janela = window.open('', '', 'width=auto, heigth=auto');
-    janela.document.write('<html><head>');
-    janela.document.write('<title>pdf</title></head>');
-    janela.document.write('<html><head>');
-    janela.document.write('<body>');
-    janela.document.write(pegar_dados);
-    janela.document.write('</body></html>');
-    janela.document.close();
-    janela.print();
-
-
-    
-}
