0f7c131f6e5d2e254214993de87c542043729257
Rafael Lima <87089861+comrafaellima@users.noreply.github.com>
2022-10-10 20:53:28 -0300

    Organizando pastas, modificando classes

--- estudante/js/curriculo.js
+++ estudante/js/curriculo.js
@@ -2,12 +2,13 @@ function imprimir() {
     let nome = document.getElementById('nome').innerHTML
     let sobrenome = document.getElementById('sobrenome').innerHTML
 
+    const nomeTitle = document.title
+
     document.title = `${nome.toLowerCase()}_${sobrenome.toLowerCase()}_curriculo`
 
-    let conteudo = document.getElementsByTagName('main').innerHTML
-
     window.print()
-    
+
+    document.title = nomeTitle
 }
 
 
---
+++ estudante/js/navegacao.js
@@ -0,0 +1,64 @@
+(function () {
+    function navegarViaAjax(hash="#/curriculo.html") {
+        if (!hash) return
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        if(!link) return
+
+        let nomeTitle = document.querySelector(`[wm-link='${hash}']`).querySelector('[secao]').attributes['secao'].value
+
+        document.title = `Estudante | ${nomeTitle}`
+
+        const destino = document.querySelector('[wm-link-destino]')
+
+        const url = '/paginas' + hash.substring(1)
+
+        fetch(url)
+            .then(resp => resp.text())
+            .then(html => {
+                destino.innerHTML = html
+                const resultado = html.match(/\<script\>([\s\S]*)\<\/script\>/)
+                if(resultado && resultado.length >= 2) {
+                    eval(resultado[1])
+                }
+            })
+    }
+
+    function configurarLinks() {
+        document.querySelectorAll('[wm-link]')
+            .forEach(link => {
+                link.href = link.attributes['wm-link'].value
+            })
+    }
+
+    function navegacaoInicial() {
+        if (location.hash) {
+            navegarViaAjax(location.hash)
+        } else {
+            const primeiroLink = document.querySelector('[wm-link="#/curriculo.html"]')
+            navegarViaAjax(primeiroLink.hash)
+        }
+    }
+
+    function mudarTitle() {
+        document.querySelectorAll('span [secao]')
+            .forEach(titulo => {
+                console.log(titulo)
+            })
+    }
+
+    window.onhashchange = e => navegarViaAjax(location.hash)
+    
+    configurarLinks()
+    navegacaoInicial()
+})()
+
+$(function(){
+    $('ul li button').click(function(i){
+        $('ul li button').removeClass('active');
+        $(this).addClass('active');
+        $('.content').each(function(index) {
+            $(this).toggleClass('active');
+        });
+    });
+});
